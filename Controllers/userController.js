fae7e26475f772adea6118a1c6202b8b31d83abb
soumia elyakni <selyakni@gmail.com>
2022-10-16 16:26:28 +0100

    modifier la fonction register

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,10 @@
     const User = require('../Models/User');
     const register = (req,res) => {
-    
+        const user = User({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password,
+        });
 }
 
 module.exports = register;
