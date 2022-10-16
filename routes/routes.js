d9b8b64ed1590c61f0138f04847af86ef7bd9106
soumia elyakni <selyakni@gmail.com>
2022-10-16 16:06:21 +0100

    importer User Schema au fichier route

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../Models/User');
 const  register= require('../controllers/userController.js');
 
 router.post('/register', register);
