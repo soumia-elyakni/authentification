0e33f9ff31ce14fe39acee275d1ca38f48c38407
soumia elyakni <selyakni@gmail.com>
2022-10-16 15:54:13 +0100

    Create user schema

---
+++ Models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 5,
+        max: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 11,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min:8
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
