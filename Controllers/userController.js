1f21a2845243a2fe950d770a298d0802dc325f07
soumia elyakni <selyakni@gmail.com>
2022-10-17 22:14:27 +0100

    install packages bcryptjs

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,14 +1,22 @@
     const User = require('../Models/User');
-    const register = (req,res) => {
-       
 
+    const register = (req,res) => {
+     const {body}=req
+     User.findOne({email:req.body.email}).then(e=>{
+      if(!e){
+        User.create({...body}).then(()=>{
+          res.send('saved')
+        }).catch(()=>{
+          res.send('not saved')
+        })
+      }else{
+        res.send('email exist already')
+      }
+     })
       
-      const {body}=req
-       User.create({...body}).then(()=>{
-        res.json({body})
-       }).catch(()=>{
-        res.send('not saved')
-       });
+    
+      
+      
     
          
 }
