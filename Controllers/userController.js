9d5b6899c9551c0b1a894de42abd6ca8f509f531
soumia elyakni <selyakni@gmail.com>
2022-10-17 14:14:30 +0100

    remove validate

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,12 +1,8 @@
     const User = require('../Models/User');
-    const Joi = require('@hapi/joi');
-    const userValidSchema = require('../Validate/userValidate.js')
-
     const register = (req,res) => {
        
-      const validation = Joi.validate(req.body, userValidSchema);
-      res.send(validation);
 
+      
       const {body}=req
        User.create({...body}).then(()=>{
         res.json({body})
