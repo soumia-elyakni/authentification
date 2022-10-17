8a619f979b9b0259bdce7b6fe58b46680c17a7b1
soumia elyakni <selyakni@gmail.com>
2022-10-17 10:30:29 +0100

    add dotenv importation et express.urlencoded

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const dataBase = require('./database/db.js');
-
-mongoose.connect(dataBase, () => console.log('connected'));
-
-
 const route = require('./routes/routes.js');
+const dataBase = require('./database/db');
+const dotenv = require('dotenv');
+
+// dotenv.config();
+
+mongoose.connect(dataBase,() => console.log('connected'));
+
+
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
 
 //Route Midelwares 
 app.use('/api/user', route)
