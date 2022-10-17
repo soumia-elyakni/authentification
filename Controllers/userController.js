c1600e7fdc626b2d03c9452263d741c6a2df6ae7
soumia elyakni <selyakni@gmail.com>
2022-10-17 23:20:00 +0100

    add hash function

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,14 +1,22 @@
     const User = require('../Models/User');
+    const bcrypt = require('bcryptjs'); 
 
     const register = (req,res) => {
-     const {body}=req
+     
+     const {body}=req;
+
      User.findOne({email:req.body.email}).then(e=>{
       if(!e){
+        bcrypt.hash(body.password,10).then(e=>{
+        body.password=e;
         User.create({...body}).then(()=>{
           res.send('saved')
         }).catch(()=>{
           res.send('not saved')
         })
+      }).catch(()=>{
+        res.send('error password')
+    })
       }else{
         res.send('email exist already')
       }
