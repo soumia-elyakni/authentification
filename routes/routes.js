158616b9d2a6c269ce968ae76d74dc3f03955888
soumia elyakni <selyakni@gmail.com>
2022-10-20 10:11:18 +0100

    add user get router

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const user = require('../controllers/userController.js');
 const router = express.Router();
+const verify = require('./tokenVerify.js');
 
 
 router.post('/register', user.register );
-router.post('/login', user.login) 
+router.post('/login', user.login);
+router.get('/get', verify, user.get );
 
 
 module.exports = router;
