b706da06de3f8798ffe926e322c4e7d44e9b0525
soumia elyakni <selyakni@gmail.com>
2022-10-20 10:11:53 +0100

    add token verify function

--- routes/tokenVerify.js
+++ routes/tokenVerify.js
@@ -1,6 +1,6 @@
 const jwt= require('jsonwebtoken');
 
-function auth (req, res, next){
+const auth = (req, res, next)=>{
     const token = req.header('auth-token');
     if(!token) return res.status(401).send('Access denied')
 
@@ -13,3 +13,5 @@ function auth (req, res, next){
 
 
 }
+
+module.exports = auth;
